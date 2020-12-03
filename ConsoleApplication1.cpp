1536af410293274d872a6eeaec5b532a3b25cbd1
IskanderCtrl <75417037+IskanderCtrl@users.noreply.github.com>
2020-12-03 11:28:24 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,221 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+
+
+
+using namespace std;
+
+struct Tr
+{
+	int k;
+	int w;
+	Tr* left, * right;
+};
+
+Tr* tree = NULL;
+
+
+//Добавить элемент
+void push(int a, Tr** t)
+{
+	if ((*t) == NULL)
+	{
+		(*t) = new Tr;
+		(*t)->k = a;
+		int r;
+		r = rand() % 3 - 1;
+		(*t)->w = r;
+		(*t)->left = (*t)->right = NULL;
+		return;
+	}
+	else
+		if (a > (*t)->k)
+			push(a, &(*t)->right);
+		else
+			push(a, &(*t)->left);
+}
+
+//Bывод
+void print(Tr* t, int u)
+{
+	if (t == NULL)
+		return;
+	else
+	{
+		print(t->left, ++u);
+		for (int i = 0; i < u; ++i)
+			cout << "   ";
+		cout << t->k << endl;
+		u--;
+	}
+	print(t->right, ++u);
+}
+
+int m = -1;
+
+//Найти максимальный элемент, вес которог равен 1;
+void max(Tr* t)
+{
+	if (t == NULL)
+		return;
+	else
+	{
+		max(t->left);
+		if (t->k > m&& t->w == 1)
+			m = t->k;
+	}
+	max(t->right);
+}
+
+//Bывод с весами
+void printw(Tr* t, int u)
+{
+	if (t == NULL)
+		return;
+	else
+	{
+		printw(t->left, ++u);
+		for (int i = 0; i < u; ++i)
+			cout << "       ";
+		cout << t->k << "(" << t->w << ")" << endl;
+		u--;
+	}
+	printw(t->right, ++u);
+}
+
+
+//Удаление элемента
+Tr* del(Tr* node, int val)
+{
+	if (node == NULL)
+		return node;
+
+	if (val == node->k)
+	{
+		Tr* q;
+		if (node->right == NULL)
+			q = node->left;
+		else
+		{
+			Tr* r = node->right;
+			if (r->left == NULL)
+			{
+				r->left = node->left;
+				q = r;
+			}
+			else
+			{
+				Tr* m = r->left;
+				while (m->left != NULL)
+				{
+					r = m;
+					m = r->left;
+				}
+				r->left = m->right;
+				m->left = node->left;
+				m->right = node->right;
+				q = m;
+			}
+		}
+		delete node;
+		return q;
+	}
+	else if (val < node->k)
+		node->left = del(node->left, val);
+	else
+		node->right = del(node->right, val);
+	return node;
+}
+
+
+int main()
+{
+	int n;
+	n = 7;
+	int s;
+
+	for (int i = 0; i < n; ++i)
+	{
+		s = rand() % 30;
+		push(s, &tree);
+	}
+
+	cout << "1. push" << endl;
+	cout << "2. delete" << endl;
+	cout << "3. print" << endl;
+	cout << "4. print with weight" << endl;
+	cout << "5. max with >0 weight" << endl;
+	cout << "6. exit" << endl;
+
+	cout << endl;
+	int c;
+	cout << "Enter:";
+	cin >> c;
+
+	int p;
+	int y;
+	while (c != 0)
+	{
+		switch (c)
+		{
+		case 1: //Добавить элемент
+		{
+			cin >> p;
+			push(p, &tree);
+			cout << endl;
+			break;
+		}
+		case 2: //Удалить элемент
+		{
+			//f = 1;
+			cin >> y;
+			tree = del(tree, y);
+			cout << endl;
+			break;
+		}
+		case 3:  //Вывод без весов
+		{
+			print(tree, 0);
+			cout << endl;
+			break;
+		}
+		case 4: //Вывод с весами
+		{
+			printw(tree, 0);
+			cout << endl;
+			break;
+		}
+		case 5:  //Наибольший с положительным весом
+		{
+			m = -1;
+			max(tree);
+			cout << m << endl;
+			cout << endl;
+			break;
+		}
+		case 6: //Выход
+		{
+			return 0;
+		}
+		default:
+		{
+			cout << "Mistake((" << endl;
+			break;
+		}
+		}
+		cout << "Enter:";
+		cin >> c;
+	}
+	return 0;
+}
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
